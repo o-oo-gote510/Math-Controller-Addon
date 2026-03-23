d8fa0819b328c8f710eca2d8cedd2131d865b354
o_oo_gote510 <o.oo.gote510@gmail.com>
2026-03-24 05:46:18 +0900

    Update __init__.py
    CG_math_0.4.5
    ->
    MC_0.4.5

--- __init__.py
+++ __init__.py
@@ -14,8 +14,8 @@
 # You should have received a copy of the GNU General Public License
 ###
 
-
-#===========CG_math_0.4.5=================#
+#======Math Controller addon======#
+#===========MC_0.4.5=================#
 import bpy
 from bpy.app.translations import pgettext_iface
 import traceback
